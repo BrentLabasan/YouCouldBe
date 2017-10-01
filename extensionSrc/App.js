8754738e4c7521f05915ecaaa80110efb9eea7e5
Brent Labasan <BrentLabasan@gmail.com>
2017-10-01 15:16:46 -0700

    blocker view implemented

--- extensionSrc/App.js
+++ extensionSrc/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import Timer from './Timer';
+import Blocker from './views/blocker';
 const URL = window.location.hostname;
 const MULTIPLIER = 5;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      view: 'blocker'
+    };
+  }
 
   render() {
 
     return (
-      <div id="Brent" className="App">
+      <MuiThemeProvider>
+        <div id="#ycb-container" className="App">
           <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
-          <h1>To get started, edit <code>src/App.js</code> and save to reload.</h1>
+          {this.state.view === 'blocker' && <Blocker/>}
+
+
+
           <Timer/>
-      </div>
+          <RaisedButton label="Default" />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ extensionSrc/views/blocker.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class Blocker extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="ycb-blocker">
+          blocker view
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
