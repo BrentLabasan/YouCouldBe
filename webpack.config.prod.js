647c02e8285a8bb64bd740c1fd2643486aea7946
Brent Labasan <BrentLabasan@gmail.com>
2017-10-01 10:07:00 -0700

    18-3

---
+++ webpack.config.prod.js
@@ -0,0 +1,30 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: { // doesn't actually generate any physical files, it will just create a bundle in memory and serve it to browser. But we need to define the path and name so it can simulate the file's existence.
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    // Eliminate duplicate packages when generating bundle.
+    new webpack.optimize.DedupePlugin(),
+    
+        // Minify JS
+        new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [ // loaders teach Webpack how to handle different file types
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
