2580afbeaf6440b00a8a632593d8a28dc1c98074
Brent Labasan <BrentLabasan@gmail.com>
2017-10-01 12:35:29 -0700

    SUCCESS React published to view BUT getting weird error in console Warning: Expected server HTML to contain a matching <h1> in <div>.

---
+++ extensionSrc/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>To get started, edit <code>src/App.js</code> and save to reload.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- extensionSrc/index.js
+++ extensionSrc/index.js
@@ -7,8 +7,15 @@ import viewBlocker from './view-blocker';
 import viewOptions from './view-options';
 import jsx from './jsx.jsx';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementsByTagName("BODY"));
