8a4bba77aa08f71830a22af2f2178950b23b0815
Brent Labasan <BrentLabasan@gmail.com>
2017-10-01 12:55:00 -0700

    SUCCESS React app rendered into body...does give this useful warning though
    warning.js:33 Warning: render(): Rendering components directly into document.body is discouraged, since its children are often manipulated by third-party scripts and browser extensions. This may lead to subtle reconciliation issues. Try rendering into a container element created for your app.

--- extensionSrc/App.js
+++ extensionSrc/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="Brent" className="App">
           <h1>To get started, edit <code>src/App.js</code> and save to reload.</h1>
       </div>
     );
--- extensionSrc/index.js
+++ extensionSrc/index.js
@@ -6,6 +6,7 @@ import constants from './constants';
 import viewBlocker from './view-blocker';
 import viewOptions from './view-options';
 import jsx from './jsx.jsx';
+import jQuery from 'jQuery';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -17,5 +18,13 @@ const courseValue = numeral(1000).format('$0,0.00');
 debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<App />, document.getElementsByTagName("BODY"));
+var elYcbParent = document.createElement("div");        
+elYcbParent.id = "ycb-elYcbParent";
+document.body.appendChild(elYcbParent); // attaches view-blocker to <body> 
+
+jQuery( document ).ready(function() {
+    // ReactDOM.hydrate(<App />, document.getElementById('ycb-elYcbParent'));
+    ReactDOM.render(<App />, document.getElementsByTagName('body')[0]);    
+    console.log( "ready!" );
+});
+
