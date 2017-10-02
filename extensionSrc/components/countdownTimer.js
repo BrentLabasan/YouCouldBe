63751be43d2394d48606a0c860c78dc94744cba2
Brent Labasan <BrentLabasan@gmail.com>
2017-10-01 21:06:54 -0700

    deleted unused files

--- extensionSrc/components/countdownTimer.js
+++ extensionSrc/components/countdownTimer.js
@@ -3,13 +3,33 @@ import React, { Component } from 'react';
 export default class CountdownTimer extends React.Component {
     constructor(props) {
         super(props);
+        console.log("CountdownTimer");
+        console.log(this.props);
+        this.state = {
+            seconds: props.siteVisitCount
+        };
+    }
+
+    tick() {
+        this.setState((prevState) => ({
+            seconds: prevState.seconds - 1
+        }));
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     render() {
 
         return (
             <span id="ycb-countdownTimer">
-                {this.props.siteVisitCount}
+                {this.props.siteVisitCount} <br />
+                {this.state.seconds}
             </span>
         );
     }
