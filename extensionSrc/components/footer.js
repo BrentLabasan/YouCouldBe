0e84a2bdedaaa4fa69034b2a360cd47fd8cce8f5
Brent Labasan <BrentLabasan@gmail.com>
2017-10-01 17:37:25 -0700

    clicking on buttons shows view in alert

--- extensionSrc/components/footer.js
+++ extensionSrc/components/footer.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Footer extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  setView() {
-    alert();
+  setView(view) {
+    alert(view);
   }
 
   render() {
     return (
       <footer id="ycb-blocker">
         <ul>
-          <li onClick={this.setView}>ALTERNATIVES</li>
-          <li>OPTIONS</li>
-          <li>HELP</li>
-          <li>ABOUT</li>
-          <li>DONATE</li>
+          <li onClick={() => this.setView('alternatives')}><FlatButton label="ALTERNATIVES" /></li>
+          <li onClick={() => this.setView('options')}><FlatButton label="OPTIONS" /></li>
+          <li onClick={() => this.setView('help')}><FlatButton label="HELP" /></li>
+          <li onClick={() => this.setView('about')}><FlatButton label="ABOUT" /></li>
+          <li onClick={() => this.setView('donate')}><FlatButton label="DONATE" /></li>
         </ul>
       </footer>
     );
