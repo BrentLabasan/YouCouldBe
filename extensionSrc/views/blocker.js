f5bf1a01b9db1f128a0cea374e2f687717c434ff
Brent Labasan <BrentLabasan@gmail.com>
2017-10-02 09:59:13 -0700

    CHECKPOINT before conditionally showing Proceed button

--- extensionSrc/views/blocker.js
+++ extensionSrc/views/blocker.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import CountdownTimer from '../components/countdownTimer';
 
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+
 const URL = window.location.hostname;
+
+const style = {
+  margin: 12,
+};
+
 
 export default class Blocker extends React.Component {
   constructor(props) {
@@ -31,6 +40,12 @@ export default class Blocker extends React.Component {
           <span className="txtAlternative">CHECKING YOUR EMAIL</span>
           <h2>instead of wasting your life on {this.props.currentHostname} for the {this.getGetOrdinal(this.props.db[this.props.currentHostname].count)} time today.</h2>
           <h2>You'll be taken to {URL} in <CountdownTimer handleCountdownEnded={this.handleCountdownEnded} siteVisitCount={this.props.db[URL].count}/> seconds. In the meantime, do something productive, like cleaning your room!</h2>
+          
+          {/* <RaisedButton label="Proceed &gt;" primary={true} style={style} />
+          <FlatButton label="Primary" primary={true} />
+          <IconButton iconClassName="muidocs-icon-custom-github" /> */}
+
+          <button className="btn-proceed">PROCEED &gt;</button>
       </div>
     );
   }
