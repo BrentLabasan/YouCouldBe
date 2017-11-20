95117375fb100973bf686cd2f478f840f56ae2f5
Brent Labasan <BrentLabasan@gmail.com>
2017-11-19 20:11:04 -0800

    built out stuff for alt activities

--- extensionSrc/constants.js
+++ extensionSrc/constants.js
@@ -1,16 +1,30 @@
+// 0 one time | 1 daily | 2 weekdays only | 3 weekends only
 export const DEFAULT_ALTERNATIVE_ACTIVITIES = [
-  { name: "practice singing", lenSec: 300, tags: [''] },
+  {name: "go grocery shopping ONE TIME", lenSec: 300, tags: [''], frequency: 0 },
+  {name: "wash your car ONE TIME", lenMin: 300, tags: [''], frequency: 0},
+  {name: "clean stove range ONE TIME", lenMin: 300, tags: [''], frequency: 0},
+  {name: "mop kitchen floor ONE TIME", lenMin: 300, tags: [''], frequency: 0},
 
-  { name: "wash the dishes", lenSec: 300, tags: ['chore'] },
-  { name: "take the trash out", lenSec: 180, tags: ['chore'] },
 
-  { name: "walk around the block", lenSec: 300, tags: ['exercise'] },
+  { name: "practice singing", lenSec: 300, tags: [''], frequency: 1 },
+  { name: "wash the dishes", lenSec: 300, tags: ['chore'], frequency: 1 },
+  { name: "take the trash out", lenSec: 180, tags: ['chore'], frequency: 1 },
+  { name: "walk around the block", lenSec: 300, tags: ['exercise'], frequency: 1 },
+  { name: "standing pike", lenSec: 60, tags: ['stretch'], frequency: 1 },
+  { name: "meditate", lenSec: 300, tags: ['mental health'], frequency: 1 },
 
-  { name: "standing pike", lenSec: 60, tags: ['stretch'] },
+  { name: "pack lunch for work ON THE WEEKDAYS", lenSec: 300, tags: ['mental health'], frequency: 2 },
+  { name: "WEEKDAYS 1", lenSec: 300, tags: ['mental health'], frequency: 2 },
+  { name: "WEEKDAYS 2", lenSec: 300, tags: ['mental health'], frequency: 2 },
+  { name: "WEEKDAYS 3", lenSec: 300, tags: ['mental health'], frequency: 2 },
 
-  { name: "meditate", lenSec: 300, tags: ['mental health'] },
 
-  { name: "call your mom", lenSec: 300, tags: ['family'] }
+  { name: "call your mom ON THE WEEKEND", lenSec: 300, tags: ['family'], frequency: 3 }
+  { name: "WEEKENDS 1", lenSec: 300, tags: ['mental health'], frequency: 3 },
+  { name: "WEEKENDS 2", lenSec: 300, tags: ['mental health'], frequency: 3 },
+  { name: "WEEKENDS 3", lenSec: 300, tags: ['mental health'], frequency: 3 },
+
+
 ];
 
-// {name: "", lenMin: , tags: ['']},
+// {name: "", lenMin: , tags: [''], frequency: n},
